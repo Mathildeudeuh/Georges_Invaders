6f4dda6a435a35e5b603636417f3490f1cea8a70
Mathilde Degris <m.degris@etudiant.ecolemelies.fr>
2022-03-21 17:01:17 +0100

    Test script

--- Assets/Scripts/HeartManager.cs
+++ Assets/Scripts/HeartManager.cs
@@ -2,4 +2,31 @@ using UnityEngine;
 
 public class HeartManager : MonoBehaviour
 {
+    public GameObject heart;
+    private PlayerHealth playerHealth;
+
+    private void Start()
+    {
+        var player = GameObject.FindWithTag("player");
+        playerHealth = GetComponent<PlayerHealth>();
+    }
+
+    private void UpdateHeart(int heartNumber)
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Pour le premier élément et tant que le nombre d'éléments n'est pas égale à celui des coeurs, on ajoute un élément
+        for (var i = 0; i < heartNumber; i++)
+        {
+            Instantiate(heart, Vector3.zero, Quaternion.identity, transform);
+        }
+    }
+
+    private void Update()
+    {
+        UpdateHeart(playerHealth.currentHealth);
+    }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] protected SOPlayerHealth currentHealthSO;
-
+    //[SerializeField] protected SOPlayerHealth currentHealthSO;
+    public int currentHealth;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag.Equals("blabla"))
         {
-            currentHealthSO.currentHealth -= currentHealthSO.currentHealth;
+            //currentHealthSO.currentHealth -= currentHealthSO.currentHealth;
+            currentHealth -= currentHealth;
         }
     }
 }
