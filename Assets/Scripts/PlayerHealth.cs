fb48217efc73bde04c887ec0291ef1176d6bb6dc
Mathilde Degris <m.degris@etudiant.ecolemelies.fr>
2022-03-21 14:10:30 +0100

    Organisation + Create scripts we need

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    private PlayerHealthSO currentHealthSO;
+    private SOPlayerHealth currentHealthSO;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
---
+++ Assets/Scripts/Shoot/PowerUp/TwoCannons.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TwoCannons : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
