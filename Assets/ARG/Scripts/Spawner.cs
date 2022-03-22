30fc60e790e54636ada5d2e13c92e03916a7e656
AQ <arth.grousset@gmail.com>
2022-03-22 14:53:22 +0100

    Enemy spawn + life
    jlejur c bon

--- Assets/ARG/Scripts/Spawner.cs
+++ Assets/ARG/Scripts/Spawner.cs
@@ -4,40 +4,64 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Transform SpawnPoint;
     public GameObject[] enemy;
     public int index;
-    bool isRunning;
     public float minSpawnTime, maxSpawnTime;
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(SpawnCoroutine());
-        
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    public float timeSpawning;
+    /*
     private IEnumerator SpawnCoroutine()
     {
-        isRunning = true;
-        while (isRunning)
+        while (isRunning == true)
         {
-            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
             Spawn();
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
         }
-        
     }
+    private IEnumerator Timer()
+    {
+        
+        yield return new WaitForSeconds(1);
+        maxTime -= 1;
+        if (maxTime < 0.1)
+        {
+            isRunning = false;
+            maxTime = time;
+        }
 
-  
-    
+    }
+    */
+    public void StartSpawn()
+    {
+        StartCoroutine(SpawnCoroutine());
+        //Debug.Log("statcprout");
+    }
     private void Spawn()
     {
-        Instantiate(enemy[index], transform.position, Quaternion.identity) ;
-        //Instantiate(enemy, transform.position, Quaternion.identity);
+        Instantiate(enemy[index], SpawnPoint) ;
+        //Debug.Log("spon");
     }
+
+    IEnumerator SpawnCoroutine()
+    {
+        //Debug.Log("courout");
+        var startTime = Time.time;
+        while(Time.time< startTime + timeSpawning)
+        {
+            Spawn();
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+        }
+    }
+
+    /*
+    IEnumerator CoroutineTest()
+    {
+        var startTime = Time.time;
+        while(Time.time < startTime + 20f)
+        {
+            Debug.Log("Coucou");
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    */
 }
