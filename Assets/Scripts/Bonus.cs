7af302f025699fbb718786c2dd68c08295f9088f
Mathilde Degris <m.degris@etudiant.ecolemelies.fr>
2022-03-28 10:15:46 +0200

    Parameter save point

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -6,37 +6,18 @@ public class Bonus : MonoBehaviour
 {
     public GameObject powerUp;
     public GameObject powerUpUI;
-    public bool backToThePast = false;
-
     public GameObject background;
+    public GameObject[] heart;
 
+    public bool backToThePast = false;
     public bool isRewinding = false;
+
+    public int life;
+    public int savePoint;
+
     List<Vector3> positions;
 
-    public GameObject[] heart;
-    public int life;
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag.Equals("PowerUp"))
-        {
-            powerUpUI.SetActive(true);
-            backToThePast = true;
-            Debug.Log("I'VE GOT THE POWER");
-            powerUp.SetActive(false);
-        }
-
-        if (backToThePast == true)
-        {
-            Debug.Log("C'est terminé, ne bouge plus !");
-        }
-
-        if (collision.tag.Equals("Enemy"))
-        {
-            life = life - 1;
-        }
-    }
 
     void Start()
     {
@@ -55,30 +36,9 @@ public class Bonus : MonoBehaviour
         }
     }
 
-    private void Record()
-    {
-        positions.Insert(0, background.transform.position);
-    }
-
-    private void Rewind()
-    {
-        if (positions.Count > 0)
-        {
-            background.transform.position = positions[0];
-            positions.RemoveAt(0);
-            backToThePast = false;
-        }
-        else
-        {
-            StopBonus();
-        }
-
-    }
-
-
     void Update()
     {
-       if (life == 0)
+        if (life == 0)
         {
             if (backToThePast == true)
             {
@@ -104,6 +64,47 @@ public class Bonus : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("PowerUp"))
+        {
+            powerUpUI.SetActive(true);
+            backToThePast = true;
+            Debug.Log("I'VE GOT THE POWER");
+            powerUp.SetActive(false);
+        }
+
+        if (backToThePast == true)
+        {
+            Debug.Log("C'est terminé, ne bouge plus !");
+        }
+
+        if (collision.tag.Equals("Enemy"))
+        {
+            life = life - 1;
+        }
+    }
+
+    private void Record()
+    {
+        positions.Insert(0, background.transform.position);
+    }
+
+
+    private void Rewind()
+    {
+        if (positions.Count > savePoint)
+        {
+            background.transform.position = positions[0];
+            positions.RemoveAt(0);
+            backToThePast = false;
+        }
+        else
+        {
+            StopBonus();
+        }
+    }
+    
     private void StartBonus()
     {
         isRewinding = true;
